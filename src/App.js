254441775497aadf10cecbb66175e4aa00e13f0e
SimonS89 <stripero_088@hotmail.com>
2022-03-13 12:00:59 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavBoots } from "./components/Bootstrap/NavBoots";
 
 const persona = {
   nombre: "Juanito",
@@ -12,6 +15,7 @@ function App() {
     <div>
       <Navbar />
       <ItemListContainer persona={persona} />
+      <NavBoots />
     </div>
   );
 }
