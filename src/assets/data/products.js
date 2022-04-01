af2be8d409f5f9c27b4c8a846cf1fe2a5f999c05
SimonS89 <stripero_088@hotmail.com>
2022-04-01 20:26:30 -0300

    Add pics

--- src/assets/data/products.js
+++ src/assets/data/products.js
@@ -4,7 +4,7 @@ export const products = [
     title: "Sansevieria",
     price: 650,
     pictureAlt: "sansevieria",
-    img: "https://picsum.photos/200",
+    img: "https://i.postimg.cc/qNsgB6Pn/Sansevieria.jpg",
     stock: 2,
     detail:
       "La sansevieria, también conocida como lengua de suegra, es una de las plantas recomendadas por la NASA (Administración Nacional de la Aeronáutica y del Espacio de Estados Unidos) para eliminar del aire que respiramos tóxicos como el benceno, el tolueno, el xileno, el tricloroetileno y el formaldehído. Y añade a esto que es muy resistente a las plagas; incluso los ácaros lo tienen difícil con ella. Por todo ello, está el top ten de las que no pueden faltar en tu casa si eres principiante.",
@@ -15,7 +15,7 @@ export const products = [
     title: "Calathea",
     price: 450,
     pictureAlt: "calathea",
-    img: "https://picsum.photos/201",
+    img: "https://i.postimg.cc/7G6LNsfM/Calathea.jpg",
     stock: 6,
     detail:
       "La calatea (su nombre científico es Calathea) es una planta de interior que no tiene flores, pero ni falta que le hace con sus vistosas hojas onduladas y bicolor. Y es que mientras el anverso luce unos patrones geométricos normalmente en verde oscuro en el reverso es de color morado. Para su buen crecimiento, agradece una exposición de luz moderada, evitando siempre el sol directo. En cuanto al riego, debe ser abundante pero sin encharcar la tierra durante el crecimiento y moderado en invierno. ",
@@ -26,7 +26,7 @@ export const products = [
     title: "Peperomia Nevada",
     price: 1250,
     pictureAlt: "peperomia nevada",
-    img: "https://picsum.photos/202",
+    img: "https://i.postimg.cc/CZw5RL02/Peperomia.jpg",
     stock: 4,
     detail:
       "Porotos verdes? no! Peperomia en su máxima expresión! Estas maravillas son bastante diferentes de las pepes más tradicionales, pero iguales de hermosas! Retienen mucha agua en sus hojas por lo que podrás olvidarte del riego por más tiempo.",
@@ -37,7 +37,7 @@ export const products = [
     title: "Monstera",
     price: 1350,
     pictureAlt: "monstera",
-    img: "https://picsum.photos/203",
+    img: "https://i.postimg.cc/bGLJkcNw/Monstera.jpg",
     stock: 20,
     detail:
       "Por sus características hojas grandes, brillantes y agujereadas la conocerás. La monstera (Monstera deliciosa), también conocida como Costilla de Adán, filodendro, cerimán o mano de tigre, es una planta tropical muy agradecida que necesita pocos cuidados y será la protagonista de cualquier rincón donde la coloques. Agradece la luz abundante, pero evita a toda costa el sol directo, quemaría sus hojas. No necesita mucha humedad ambiental, pero es recomendable colocar un plato aislado de la base del tiesto con agua para mantener un grado de humedad óptimo. En verano puedes pulverizar sus hojas para refrescarlas.",
@@ -48,7 +48,7 @@ export const products = [
     title: "Anthurium",
     price: 970,
     pictureAlt: "anthurium",
-    img: "https://picsum.photos/204",
+    img: "https://i.postimg.cc/FkrVGQ0C/Anthurium.jpg",
     stock: 10,
     detail:
       "Es una de las plantas más habituales en los hogares y, sin duda, sobran motivos para ello. No solo es increíblemente bella y llamativa. Además, cuando se sabe cómo cuidar de un anthurium es imposible no rendirse a sus encantos. Y es que esta preciosa especie nos lo pone sumamente fácil para desplegar su increíble colorido. Tanto que es ideal para quienes se inician en la jardinería, ya que bien puede catalogarse como una planta de bajo mantenimiento.",
@@ -59,7 +59,7 @@ export const products = [
     title: "Alocasia Dragon",
     price: 1370,
     pictureAlt: "alocasia dragon",
-    img: "https://picsum.photos/205",
+    img: "https://i.postimg.cc/kDL5xy9K/Alocasia-Dragon.jpg",
     stock: 1,
     detail:
       "¿¿En serio existe LA PLANTA con piel de dragón?? WTF!!! Mira bien sus hojas, poca gente las ha visto en persona. La secreta Alocasia Dragon, solo para los más rápidos. Fácil de cuidar, pero mejor métele un candado, te la van a querer robar. Stock muy limitado en todo el mundo.",
