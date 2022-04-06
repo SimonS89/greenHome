d88ca4e69c86755bc436ecb38b7f30ba6fd7f656
SimonS89 <stripero_088@hotmail.com>
2022-04-06 14:01:04 -0300

    Intercambiabilidad

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,43 +1,40 @@
-import { getFetch } from "../helpers/getFetch";
-import React, { useEffect, useState } from "react";
+// import { getFetch } from "../helpers/getFetch";
+// import { useEffect, useState } from "react";
 import Item from "../Item/Item";
-import Spinner from "react-bootstrap/Spinner";
-import { useParams } from "react-router-dom";
+// import Spinner from "react-bootstrap/Spinner";
+// import { useParams } from "react-router-dom";
 
-const ItemList = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
+const ItemList = ({ products }) => {
+  //   const [products, setProducts] = useState([]);
+  //   const [loading, setLoading] = useState(false);
 
-  const { categoryId } = useParams();
+  //   const { categoryId } = useParams();
 
-  useEffect(() => {
-    setLoading(true);
-    getFetch
-      .then((resp) => {
-        if (categoryId) {
-          setProducts(resp.filter((item) => item.category === categoryId));
-        } else {
-          setProducts(resp);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => setLoading(false));
-  }, [categoryId]);
+  //   useEffect(() => {
+  //     setLoading(true);
+  //     getFetch
+  //       .then((resp) => {
+  //         if (categoryId) {
+  //           setProducts(resp.filter((item) => item.category === categoryId));
+  //         } else {
+  //           setProducts(resp);
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       })
+  //       .finally(() => setLoading(false));
+  //   }, [categoryId]);
 
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center pb-5">
-      {loading ? (
-        <Spinner animation="grow" role="status" size="lg">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      ) : (
-        products.map((producto) => (
+    <>
+      <h1 className="text-center mb-5 mt-5">Productos Destacados</h1>
+      <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center pb-5">
+        {products.map((producto) => (
           <Item key={producto.id} producto={producto} />
-        ))
-      )}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,48 @@
 import ItemList from "../ItemList/ItemList";
+import { getFetch } from "../helpers/getFetch";
+import { useEffect, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import { useParams } from "react-router-dom";
 
 const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    getFetch
+      .then((resp) => {
+        if (categoryId) {
+          setProducts(resp.filter((item) => item.category === categoryId));
+        } else {
+          setProducts(resp);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
+  }, [categoryId]);
+
   return (
-    <div className="container pt-5">
-      <h1 className="text-center mb-5 mt-5">Productos Destacados</h1>
-      <ItemList />
-    </div>
+    <>
+      <div className="container pt-5 mt-5 text-center">
+        {/* <h1 className="text-center mb-5 mt-5">Productos Destacados</h1> */}
+        {loading ? (
+          <Spinner
+            animation="grow"
+            role="status"
+            className="mt-5 mb-5"
+            size="lg"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : (
+          <ItemList products={products} />
+        )}
+      </div>
+    </>
   );
 };
 
