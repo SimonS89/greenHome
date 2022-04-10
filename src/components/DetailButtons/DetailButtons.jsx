19707b9619072fbc007faa0787030bf3ac480604
SimonS89 <stripero_088@hotmail.com>
2022-04-10 12:32:03 -0300

    fix btn colors

--- src/components/DetailButtons/DetailButtons.jsx
+++ src/components/DetailButtons/DetailButtons.jsx
@@ -4,13 +4,10 @@ export const DetailButtons = () => {
   return (
     <div className="col-sm d-inline mb-3 d-flex justify-content-around ">
       {" "}
-      <Link
-        to="/cart"
-        className="btn btn-outline-primary  d-inline fw-bolder m-2"
-      >
+      <Link to="/cart" className="btn btn-primary  d-inline fw-bolder m-2">
         Terminar compra
       </Link>
-      <Link to="/" className="btn btn-outline-primary d-inline  fw-bolder m-2">
+      <Link to="/" className="btn btn-primary d-inline  fw-bolder m-2">
         Seguir comprando
       </Link>
     </div>
