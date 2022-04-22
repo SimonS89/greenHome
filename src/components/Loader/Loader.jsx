72f1c59893a663bc0e6feaf457a23e8ff837ddde
SimonS89 <stripero_088@hotmail.com>
2022-04-22 15:25:55 -0300

    loader text

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,17 +3,22 @@ import Spinner from "react-bootstrap/Spinner";
 export const Loader = () => {
   return (
     <>
-      <Spinner
-        animation="grow"
-        role="status"
-        className="mt-5 mb-5 text-dark"
-        size="lg"
-      >
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-      <p className="text-center fw-bold mb-3 fs-3 bg-light">
-        Aguarde por favor...
-      </p>
+      <div>
+        {" "}
+        <Spinner
+          animation="grow"
+          role="status"
+          className="mt-5 mb-5 text-dark"
+          size="lg"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+      <div>
+        <p className="text-center fw-bold mb-3 fs-3 bg-light">
+          Aguarde por favor...
+        </p>
+      </div>
     </>
   );
 };
